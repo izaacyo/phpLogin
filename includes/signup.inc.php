2b57b9be19fbdd9158ab0cd107688635b3c3605c
izaacyo <mariusppetrut@gmail.com>
2021-11-23 19:40:28 +0200

    created includes file

---
+++ includes/signup.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+
+if (isset($_POST["submit"])) {
+
+    $uid = $_POST["uid"];
+    $pwd = $_POST["pwd"];
+    $pwdrepeat = $_POST["pwdrepeat"];
+    $email = $_POST["email"];
+}
+
+
+// Instantiate SignupContr class
+
+// Running error handlers and user signup
+
+// Going to back to front page
